50bbd3cf0773fe0e6aa9071303bddb2d111c0472
daniel21h <hesseldainel21@gmail.com>
2020-08-26 15:43:31 -0300

    feat: refactoring catalog item

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import { IProduct } from '../store/modules/cart/types';
+import { addProductToCart } from '../store/modules/cart/actions';
+import { useDispatch } from 'react-redux';
+
+// import { Container } from './styles';
+
+interface CatalogItemProps {
+  product: IProduct;
+}
+
+const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleAddProductToCart = useCallback(() => {
+    dispatch(addProductToCart(product));
+  }, [dispatch, product])
+
+  return (
+    <article>
+      <strong>{product.title}</strong> {" - "}
+      <span>{product.price}</span> {"  "}
+
+      <button 
+        type="button"
+        onClick={handleAddProductToCart}
+      >
+        Adicionar
+      </button>
+    </article>
+  )
+}
+
+export default CatalogItem;
